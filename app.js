c626ceca0067601966b50075edf8858044096f5a
EmanuelCav <cavallinema@gmail.com>
2024-01-17 09:35:50 -0300

    Change icon and update footer

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const containerProjects = document.getElementById("container-projects")
+const containerSkills = document.getElementById("container-skills")
 
 const headerIcon = document.getElementById("header-icon")
 const containerDropDown = document.getElementById("container-dropdown")
 
-var projectsJS = [{
+const projectsJS = [{
     title: "Surveys",
     description: "Stack MERN, Redux con TypeScript.",
     image: "https://res.cloudinary.com/projects-emanuek/image/upload/v1705094965/surveys_hkztfb.png",
@@ -26,6 +27,69 @@ var projectsJS = [{
     link: "https://emails-kn32.onrender.com"
 }]
 
+const skills = [{
+    image: "https://res.cloudinary.com/projects-emanuek/image/upload/v1671473165/html_be6orf.png",
+    text: "HTML"
+}, {
+    image: "https://res.cloudinary.com/projects-emanuek/image/upload/v1671473165/css_ds4thn.png",
+    text: "CSS"
+}, {
+    image: "https://res.cloudinary.com/projects-emanuek/image/upload/v1671473165/Bootstrap_iqquha.png",
+    text: "Bootstrap"
+}, {
+    image: "https://res.cloudinary.com/projects-emanuek/image/upload/v1704825258/tailwind_dw1gyq.png",
+    text: "Tailwind"
+}, {
+    image: "https://res.cloudinary.com/projects-emanuek/image/upload/v1671473165/JavaScript_hslqmb.png",
+    text: "JavaScript"
+}, {
+    image: "https://res.cloudinary.com/projects-emanuek/image/upload/v1671473166/Typescript_vnyxg4.png",
+    text: "TypeScript"
+}, {
+    image: "https://res.cloudinary.com/projects-emanuek/image/upload/v1671473166/react_gcub8c.png",
+    text: "React"
+}, {
+    image: "https://res.cloudinary.com/projects-emanuek/image/upload/v1678728288/nextjs_pmxmgc.png",
+    text: "Next.js"
+}, {
+    image: "https://res.cloudinary.com/projects-emanuek/image/upload/v1671473166/redux_uzfihr.png",
+    text: "Redux"
+}, {
+    image: "https://res.cloudinary.com/projects-emanuek/image/upload/v1671473166/node_jzcjnl.png",
+    text: "Node.js"
+}, {
+    image: "https://res.cloudinary.com/projects-emanuek/image/upload/v1671474636/mongodb_jqr0y1.png",
+    text: "MongoDB"
+}, {
+    image: "https://res.cloudinary.com/projects-emanuek/image/upload/v1671474381/sql_yq1wrd.png",
+    text: "SQL"
+}, {
+    image: "https://res.cloudinary.com/projects-emanuek/image/upload/v1704821999/docker_pgvdcu.png",
+    text: "Docker"
+}]
+
+for (let i = 0; i < skills.length; i++) {
+
+    var containSkill = document.createElement("div")
+    containSkill.className = "contain-skill no-select"
+
+    var imageSkill = document.createElement("img")
+    imageSkill.src = skills[i].image
+    imageSkill.alt = skills[i].text
+    imageSkill.loading = "lazy"
+    imageSkill.className = "image-skill"
+
+    var textSkill = document.createElement("p")
+    textSkill.innerHTML += skills[i].text
+    textSkill.className = "text-skill"
+
+    containSkill.appendChild(imageSkill)
+    containSkill.appendChild(textSkill)
+
+    containerSkills.appendChild(containSkill)
+
+}
+
 var id = 0
 
 for (var i = 0; i < projectsJS.length; i++) {
@@ -34,7 +98,7 @@ for (var i = 0; i < projectsJS.length; i++) {
     linkProject.className = "redirect-project"
 
     var containProject = document.createElement("div")
-    containProject.className = "contain-project"
+    containProject.className = "contain-project hvr-bounce-in"
     containProject.id = id
 
     var containImageProject = document.createElement("div")
@@ -46,6 +110,7 @@ for (var i = 0; i < projectsJS.length; i++) {
     var imageContainProject = document.createElement("img")
     imageContainProject.src = projectsJS[i].image
     imageContainProject.alt = projectsJS[i].title
+    imageContainProject.loading = "lazy"
     imageContainProject.className = "image-project"
 
     var titleContainProject = document.createElement("h1")
@@ -63,7 +128,7 @@ for (var i = 0; i < projectsJS.length; i++) {
 
     var contentProjectMore = document.createElement("p")
     contentProjectMore.innerHTML += projectsJS[i].contentMore
-    contentProjectMore.className = "content-project-more"  
+    contentProjectMore.className = "content-project-more"
 
     containImageProject.appendChild(imageContainProject)
 
